5dfe694d26074c4d49af5205b02fb54251d69344
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-21 00:55:32 -0500

    added writing an article functionality

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -4,11 +4,34 @@ class TravelsController < ApplicationController
         @list_of_categories = list_of_categories
     end
 
+    def new 
+        @travel = current_user.travels.build
+    end
+
+    def create
+        @travel = current_user.travels.build(travel_params)
+        @travel.image = 'https://www.tourniagara.com/wp-content/uploads/2014/10/default-img.gif'
+
+        if @travel.save
+          redirect_to @travel, notice: 'Traveling article created successfully'
+        else
+          render :new
+        end
+    end
+
+    def show
+        @travel = Travel.find(params[:id])
+    end
+
     def featured_traveling_article
         Travel.all.includes(:votes).max_by { |t| t.votes.length }
     end
 
     def list_of_categories
         Category.all.order(:priority).take(4)
+    end
+
+    def travel_params
+        params.require(:travel).permit(:title, :text)
     end
 end
