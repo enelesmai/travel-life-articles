3edaaaaa0b165933fc2b772f5f749645a015acbd
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-25 16:01:06 -0500

    added helper to comment/form view

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,17 @@
 module CommentsHelper
+    def show_ask_author(f)
+        html = ''
+        html += '<p>'
+            
+        if current_user.nil?
+            html += f.label :author_name, "Your Name:"
+            html += '<br>'
+            html += f.text_field :author_name, class: 'form-control'
+        else
+            html += f.label :author_name, "Your Name: #{current_user.name}"
+            html += f.hidden_field :author_name, :value => current_user.name
+        end
+        html += '</p>'
+        html.html_safe
+    end
 end
