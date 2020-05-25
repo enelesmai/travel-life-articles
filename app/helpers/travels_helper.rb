020765e06c13f0d1d0b55f2f4d7fd93000353cc4
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-25 16:19:40 -0500

    added helper to travel/new view

--- app/helpers/travels_helper.rb
+++ app/helpers/travels_helper.rb
@@ -2,4 +2,15 @@ module TravelsHelper
   def travels_by_category(category)
     Category.where(category).travels.order(created_at: :desc)
   end
+  def show_errors
+    html = ''
+    if @travel.errors.any?
+      html += "<div class='alert alert-warning text-center' role='alert'>"
+        @travel.errors.full_messages.each do |msg| 
+          html += "<p>#{msg}</p>"
+        end
+      html += "</div>"
+    end
+    html.html_safe
+  end
 end
