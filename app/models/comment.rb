fcabbb47ade7f29432804df3fc3e94d03d57771a
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-25 13:02:09 -0500

    added testcases for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :travel
+  validates :author_name, presence: true, length: { minimum: 6 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'Model.comment' do
+    context 'validates comment object' do
+      subject { build(:comment) }
+      it { should validate_presence_of(:author_name) }
+      it { should validate_presence_of(:body) }
+      it { should validate_length_of(:author_name).
+               is_at_least(6)}
+      it { should validate_length_of(:body).
+        is_at_least(10)}
+    end
+  end
+end
